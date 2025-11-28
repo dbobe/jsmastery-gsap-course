97064ee198c6e6e42be6bc5118eb381c92e2b482
David Bobe <bobe.david@live.com>
2025-11-28 17:44:36 -0500

    First commit

---
+++ pages/01a_your-first-animation/script.js
@@ -0,0 +1,13 @@
+gsap.to(".card", {
+    opacity: 1,
+    scale: 1,
+    duration: 5,
+    onComplete: () => {
+        gsap.to(".card", {
+            boxShadow: "0 20px 80px rgba(29, 209, 161, 0.6)",
+            repeat: -1,
+            yoyo: true,
+            duration: 0.5,
+        });
+    },
+});
