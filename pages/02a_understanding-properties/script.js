1c88f371c760ef57213f53c727ba8a6eb10bc656
David Bobe <bobe.david@live.com>
2025-11-29 09:47:05 -0500

    understanding properties

---
+++ pages/02a_understanding-properties/script.js
@@ -0,0 +1,12 @@
+import gsap from "gsap";
+
+gsap.to(".box", {
+    opacity: 1,
+    rotation: 360,
+    background: '#ff6f61',
+    scale: 1.25,
+    duration: 2,
+    ease: "bounce",
+    // repeat: -1,
+    // yoyo: true,
+})
